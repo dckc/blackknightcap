e5f8e017beb32fb53e3191d3b84699431db3d93e
Dan Connolly <dconnolly@kumc.edu>
2013-07-26 14:43:59 -0500

    update ocap package docstring, removing vestiges of earlier context

--- ocap/__init__.py
+++ ocap/__init__.py
@@ -1,8 +1,5 @@
-'''admin_lib -- Provide HERON policy administration independent of Web layer.
-=============================================================================
-
-AuthorityInjection: Capability-based Security and Dependency Injection
-----------------------------------------------------------------------
+'''ocap -- object capability support
+====================================
 
 The `Object-capability model`__ is useful for establishing security
 properties of a large system in terms of properties of the parts:
@@ -15,17 +12,6 @@ properties of a large system in terms of properties of the parts:
 __ http://en.wikipedia.org/wiki/Object-capability_model
 __ http://en.wikipedia.org/wiki/Principle_of_least_privilege
 
-This package makes extensive use of AuthorityInjection__, which is the
-use of dependency injection in service of both separation of concerns
-(and hence testability) and secure composition.  See
-:mod:`heron_wsgi.admin_lib.rtconfig` for details on the dependency
-injection API and its use in runtime configuration.
-
-__ http://informatics.kumc.edu/work/wiki/AuthorityInjection
-
-The RunTime modules are trusted; they serve as a sort of powerbox.
-See also integration testing below.
-
 Unit Testing with doctest
 -------------------------
 
@@ -34,30 +20,10 @@ for one module::
 
   $ python -m doctest medcenter.py
 
-.. note:: You can run the tests for most (all?) of the modules in this
-          package with `tests.py`, but this is a kludge that should be
-          replaced by using nose__ and `--with-doctest`.
+If you have nose__ installed, you can test all modules::
+
+  $ nosetests --with-doctest
 
 __ http://readthedocs.org/docs/nose/en/latest/
-
-Integration Testing: running modules as scripts
------------------------------------------------
-
-Many of the modules in this package run from the command line.
-For example::
-
-  $ python medcenter.py somebody
-
-It relies on an `integration-test.ini` file (see
-:file:`integration-test.ini.example` and ask around for the various
-unpublished details.)
-
-.. todo:: document integration-test.ini better
-
-To find out the command line arguments, just run::
-
-  $ python medcenter.py
-
-and the stacktrace should give a pretty good clue.
 
 '''
